4362b67284737055be650dfe464a620551c4a7c7
Deepank Chauhan <deepankchauhan73@gmail.com>
2019-01-23 00:10:57 +0530

    Update string.c

--- string.c
+++ string.c
@@ -3,5 +3,16 @@
 
 int main(void)
 {
-
+ char a[100], b[100];
+  printf("Enter a string:");
+  gets(a);
+  
+  strcpy(b,a);
+  strrev(b);
+  
+  if (strcmp(a,b) == 0)
+    printf("palindrome\n");
+  else
+    printf("not palindrome\n");
+  return 0;
 }
